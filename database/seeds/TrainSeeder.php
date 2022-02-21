c31729452d4ba615207c69ff14b068dd0047c702
ChriEffe <christianferrazzano93@gmail.com>
2022-02-21 15:43:58 +0100

    eseguo seeder

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Train;
 
 class TrainSeeder extends Seeder
 {
@@ -9,8 +11,22 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $train = new Train();
+            $train-> company = $faker->word();
+            $train-> departure_station = $faker->city();
+            $train-> arrival_station = $faker->city();
+            $train-> departure_date = $faker->dateTimeBetween('-1 week', '+1 week');
+            $train-> arrival_date = $faker->dateTimeBetween('-1 week', '+1 week');
+            $train-> departure_time = $faker->time();
+            $train-> arrival_time = $faker->time();
+            $train-> code_train = $faker->regexify('[A-Z]{3}[0-5]{5}');
+            $train-> carriages_number = $faker->numberBetween(5,12);
+            $train-> on_time = $faker->boolean();
+            $train-> cancelled = $faker->boolean();
+            $train->save();
+        }
     }
 }
